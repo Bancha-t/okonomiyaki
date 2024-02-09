175adeee137ae45aa497b02b289ad4fe782b2a00
Bancha-t <gmtv7887@gmail.com>
2024-02-09 16:55:56 +0700

    namototoji

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
+#include <cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
-}
+double overlap(Rect *A,Rect *B){
+	double w , h , a ;
+	w = min((A->x+A->w),(B->x+B->w)) - max(A->x,B->x) ;
+	h = min(A->y,B->y)- max((A->y-A->h),(B->y-B->h)) ;
+	a = w*h ;
+	if(a <= 0) return 0 ;
+	else return a ;
+} 
